6d888878096f670f46d825f327d4d0484c98b116
ngtangtrung2004-qt <ngtangtrung2004@gmail.com>
2024-09-29 16:12:05 +0700

    fix signin

--- src/pages/Signinsignup/index.jsx
+++ src/pages/Signinsignup/index.jsx
@@ -13,7 +13,7 @@ function SignupSignin (){
         <div className={clsx('container',active && 'active')} id="container" >
             <div className="form-container sign-up ">
                 <form>
-                    <h1>Tạo tài khoản</h1>
+                    <h2>Tạo tài khoản</h2>
                     <div className="social-icons">
                         <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
                         <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
@@ -30,7 +30,7 @@ function SignupSignin (){
             </div>
             <div className="form-container sign-in">
                 <form >
-                    <h1>Đăng nhập</h1>
+                    <h2>Đăng nhập</h2>
                     <div className="social-icons">
                         <a href="#" className="icon"><i className="fa-brands fa-google-plus-g"></i></a>
                         <a href="#" className="icon"><i className="fa-brands fa-facebook-f"></i></a>
@@ -47,13 +47,13 @@ function SignupSignin (){
                 <div className="toggle">
                     <div className="toggle-panel toggle-left">
                         <img src={logo} alt="" width="100px" height="auto"/>
-                        <h1>Chào mừng bạn trở lại !</h1>
+                        <h2>Chào mừng bạn trở lại !</h2>
                         <p>Nhập thông tin cá nhân của bạn để sử dụng tất cả các tính năng của trang web</p>
                         <button className="hidden" id="login" onClick={()=>setActive(false)}>Đăng nhập</button>
                     </div>
                     <div className="toggle-panel toggle-right"> 
                         <img src={logo} alt="" width="100px" height="auto"/>
-                        <h1>Chào mừng bạn đến với Four Tech</h1>
+                        <h2>Chào mừng bạn đến với Four Tech</h2>
                         <p>Đăng ký với thông tin cá nhân của bạn để sử dụng tất cả các tính năng của trang web</p>
                         <button className="hidden" id="register" onClick={()=>setActive(true)}>Đăng ký</button>
                     </div>
