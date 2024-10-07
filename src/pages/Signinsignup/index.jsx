6fd643b1716c2d87c1e8332081e2b7aec4f2329f
truongdat2001 <dattruong792001@gmail.com>
2024-10-08 00:44:58 +0700

    fix3

--- src/pages/Signinsignup/index.jsx
+++ src/pages/Signinsignup/index.jsx
@@ -1,16 +1,16 @@
-import"./signUpSignIn.css"
-import  { useState } from 'react'
+import "./signUpSignIn.css"
+import { useState } from 'react'
 import clsx from 'clsx';
-import logo from "../../../public/Logo.png"
+import logo from "/Logo.png"
 import { Link } from "react-router-dom";
 
 
-function SignupSignin (){
+function SignupSignin() {
     const [active, setActive] = useState(false)
-    
-    return(
-        
-        <div className={clsx('container',active && 'active')} id="container" >
+
+    return (
+
+        <div className={clsx('container', active && 'active')} id="container" >
             <div className="form-container sign-up ">
                 <form>
                     <h2>Tạo tài khoản</h2>
@@ -20,10 +20,10 @@ function SignupSignin (){
                         <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
                         <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
                     </div>
-                    <input type="text" placeholder="Tên đăng nhập "/>
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Mật khẩu"/>
-                    <input type="password" placeholder="Nhập lại mật khẩu"/>
+                    <input type="text" placeholder="Tên đăng nhập " />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Mật khẩu" />
+                    <input type="password" placeholder="Nhập lại mật khẩu" />
 
                     <button>Đăng ký</button>
                 </form>
@@ -37,8 +37,8 @@ function SignupSignin (){
                         <a href="#" className="icon"><i className="fa-brands fa-github"></i></a>
                         <a href="#" className="icon"><i className="fa-brands fa-linkedin-in"></i></a>
                     </div>
-                    <input type="email" placeholder="Tên đăng nhập"/>
-                    <input type="password" placeholder="Mật khẩu"/>
+                    <input type="email" placeholder="Tên đăng nhập" />
+                    <input type="password" placeholder="Mật khẩu" />
                     <Link to='/forgotPassword' >Quên mật khẩu!</Link>
                     <button>Đăng nhập</button>
                 </form>
@@ -46,21 +46,21 @@ function SignupSignin (){
             <div className="toggle-container">
                 <div className="toggle">
                     <div className="toggle-panel toggle-left">
-                        <img src={logo} alt="" width="100px" height="auto"/>
+                        <img src={logo} alt="" width="100px" height="auto" />
                         <h2>Chào mừng bạn trở lại !</h2>
                         <p>Nhập thông tin cá nhân của bạn để sử dụng tất cả các tính năng của trang web</p>
-                        <button className="hidden" id="login" onClick={()=>setActive(false)}>Đăng nhập</button>
+                        <button className="hidden" id="login" onClick={() => setActive(false)}>Đăng nhập</button>
                     </div>
-                    <div className="toggle-panel toggle-right"> 
-                        <img src={logo} alt="" width="100px" height="auto"/>
+                    <div className="toggle-panel toggle-right">
+                        <img src={logo} alt="" width="100px" height="auto" />
                         <h2>Chào mừng bạn đến với Four Tech</h2>
                         <p>Đăng ký với thông tin cá nhân của bạn để sử dụng tất cả các tính năng của trang web</p>
-                        <button className="hidden" id="register" onClick={()=>setActive(true)}>Đăng ký</button>
+                        <button className="hidden" id="register" onClick={() => setActive(true)}>Đăng ký</button>
                     </div>
                 </div>
             </div>
         </div>
-    
+
     )
 }
 
