0cac1215949ebeda02d5167bcb3d73b8def058c0
tranhoanghaiiiiii <hoanghaihyh@gmail.com>
2024-10-14 02:24:24 +0700

    fix lỗi routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,12 +3,8 @@ import SignupSignin from "../pages/User/Signinsignup"
 import HomePage from "../pages/User/Home"
 import Voucher from '../components/Voucher/Voucher'
 import Event from "../components/Event/Event"
-import Item_headphone from "../components/Item_headphones/Item_headphone"
-<<<<<<< HEAD
 import ProductDetails from "../pages/User/ProductDetails"
-=======
 import Cart from "../pages/User/Cart/Cart"
->>>>>>> 66d3f5478140518aeb94dd68694361c02afca2d8
 
 const publicRoutes = [
     {path: "/", component: HomePage},
@@ -16,14 +12,9 @@ const publicRoutes = [
     {path: '/signin', component: SignupSignin, layout: null},
     {path: '/voucher', component: Voucher},
     {path: '/event', component: Event},
-<<<<<<< HEAD
     {path: '/detail', component: ProductDetails},
-    {path: '/item_headphone', component: Item_headphone}
-=======
-    {path: '/item_headphone', component: Item_headphone},
     {path: '/cart', component: Cart}
 
->>>>>>> 66d3f5478140518aeb94dd68694361c02afca2d8
 ]
 
 
